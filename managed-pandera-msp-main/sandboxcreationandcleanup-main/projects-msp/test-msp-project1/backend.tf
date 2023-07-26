af0d5e9c3b01a0a4737b8fbf3b5b0993dc763218
nagesh66 <132870594+nagesh66@users.noreply.github.com>
2023-07-26 10:09:34 +0000

    checking cloud build

--- managed-pandera-msp-main/sandboxcreationandcleanup-main/projects-msp/test-msp-project1/backend.tf
+++ managed-pandera-msp-main/sandboxcreationandcleanup-main/projects-msp/test-msp-project1/backend.tf
@@ -1,6 +1,6 @@
-terraform {
-  backend "gcs" {
-    bucket = "msp-managed-admin-state-bucket"
-    prefix = "/test-msp-project1"
-  }
-}
+#terraform {
+#  backend "gcs" {
+#    bucket = "msp-managed-admin-state-bucket"
+#    prefix = "/test-msp-project1"
+#  }
+#}
